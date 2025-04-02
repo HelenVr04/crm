8672c155d591818a6dffa2bf61a3dad2cd300091
Glezz888 <alexisig.ti22@utsjr.edu.mx>
2025-04-02 14:50:18 -0600

    diseno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ClienteComponent } from './pages/cliente/cliente.component';
 import { ProductoComponent } from './pages/producto/producto.component';
-<<<<<<< HEAD
+
 import { PedidoComponent } from './pages/pedido/pedido.component';
 import { ProveedorComponent } from './pages/proveedor/proveedor.component';
-=======
+
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './auth.guard';
-
->>>>>>> 88b60777c5a608024759915458cd402c9dc99f9c
 
 export const routes: Routes = [
     {
@@ -31,7 +29,7 @@ export const routes: Routes = [
         path:'inventario',
         component: ProductoComponent,
         canActivate: [authGuard]
-        
+
     },
     {
         path: 'pedidos',
